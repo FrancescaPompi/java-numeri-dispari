e2c3dc13e3c281b5619bfece9daa119634943997
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-18 16:01:05 +0100

    messi numeri casuali in array

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -1,6 +1,6 @@
 package org.generation.italy.cicli;
 
-
+import java.util.Random;
 import java.util.Scanner;
 
 public class StampaNumeriDispari {
@@ -8,12 +8,22 @@ public class StampaNumeriDispari {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		int n;
+		// interazione con l'utente
 		System.out.print("Scegli la lunghezza dell'array: ");
-		n = scanner.nextInt();
-		System.out.print("Hai scelto: " + n);
+		int n = scanner.nextInt();
+		System.out.println("Hai scelto: " + n);
+		
+		System.out.println("Creo un array con " + n + " numeri naturali casuali: ");
 		
 		int[] numeriNaturali = new int [n];
+		Random random = new Random();
+		
+		for(int i = 0; i < numeriNaturali.length; i++) {
+			int numeriCasuali = random.nextInt(100) + 0;
+			numeriNaturali[i] = numeriCasuali;
+			System.out.print(numeriNaturali[i] + " ");
+		}
+		
 		
 		
 		scanner.close();
