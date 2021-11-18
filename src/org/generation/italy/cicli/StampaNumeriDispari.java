cce6c4b581ecfe4d03f85dd3adfd4f148a7db328
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-18 15:20:50 +0100

    creato array con input utente

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -1,10 +1,23 @@
 package org.generation.italy.cicli;
 
+
+import java.util.Scanner;
+
 public class StampaNumeriDispari {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
-
+		int n;
+		System.out.print("Scegli la lunghezza dell'array: ");
+		n = scanner.nextInt();
+		System.out.print("Hai scelto: " + n);
+		
+		int[] numeriNaturali = new int [n];
+		
+		
+		scanner.close();
 	}
+	
 
 }
